53ce976f59552326671a316cd37296d8201fe205
Phuc Le T.T. VN.Danang <phuc.le@MBA0329.local>
2021-06-12 10:50:37 +0700

    exercise 2

--- react-app/src/header.js
+++ react-app/src/header.js
@@ -3,28 +3,28 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <header class="page-header">
-        <div class="container flex-space-between">
-          <div class="header-logo">
+      <header className="page-header">
+        <div className="container flex-space-between">
+          <div className="header-logo">
             <a href="#"><img src="./logo.svg" alt="Logo"/></a>
           </div>
-          <nav class="navbar">
-            <ul class="nav-list">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Home</a>
+          <nav className="navbar">
+            <ul className="nav-list">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Category</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Category</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Sale</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Sale</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
               </li>
             </ul>
           </nav>
-          <ul class="social-list">
+          <ul className="social-list">
             <li><a href="#"><img src="./search.svg" alt="Logo" /></a></li>
             <li><a href="#"><img src="./login.svg" alt="Logo" /></a></li>
           </ul>
