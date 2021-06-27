b59cff6bba232c8e6a32b1fedb6f5377c070e68e
Phuc Le T.T <phuc.le@monstar-lab.com>
2021-06-27 09:05:44 +0700

    bt4 form

--- react-app/src/footer.js
+++ react-app/src/footer.js
@@ -4,17 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="page-footer flex-centered">
-        {/* <div class="footer-left">
-          <img src="./cactus.png" alt="Logo" />
-        </div> */}
         <p>Copy Right @2021 TemplateHouse</p>
-        {/* <div class="footer-right">
-          <ul class="social-list">
-            <li><a href="#"><img src="./search.svg" alt="Logo" /></a></li>
-            <li><a href="#"><img src="./login.svg" alt="Logo" /></a></li>
-            <li><a href="#"><img src="./search.svg" alt="Logo" /></a></li>
-          </ul>
-        </div> */}
       </footer>
     )
   }
