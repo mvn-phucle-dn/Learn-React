038043dcfe23ccb59ceb5515b7b63b365ce54e94
Phuc Le T.T <phuc.le@monstar-lab.com>
2021-08-09 20:54:09 +0700

    lazy load - Redux

--- react-app/src/pages/Features/Home/index.js
+++ react-app/src/pages/Features/Home/index.js
@@ -1,17 +1,91 @@
-import React, {  } from 'react';
+import React, { useState } from 'react';
+import { FaHeart } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { toggle } from '../../../store/favSlice';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const [news, setNews] = useState([
+    {
+      id: 1,
+      title: 'This is title 1',
+      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+      img: './yibo.jpeg',
+      favs: false
+    },
+    {
+      id: 2,
+      title: 'This is title 2',
+      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+      img: './yibo.jpeg',
+      favs: false
+    },
+    {
+      id: 3,
+      title: 'This is title 3',
+      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+      img: './yibo.jpeg',
+      favs: false
+    },
+    {
+      id: 4,
+      title: 'This is title 4',
+      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+      img: './yibo.jpeg',
+      favs: false
+    },
+    {
+      id: 5,
+      title: 'This is title 5',
+      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+      img: './yibo.jpeg',
+      favs: false
+    }
+  ]);
+
+  function handleRemove(id) {
+    const newList = news.filter(e => e.id !== id);
+    setNews(newList);
+  }
+
+  const handleFav = (e, id) => {
+    e.preventDefault();
+    const newProds = news.map(e => {
+      if (e.id === id) {
+        e.favs = !e.favs;
+      }
+      return e;
+    })
+    setNews(newProds);
+    dispatch(toggle(id));
+  }
+  
   return (
     <div className="container">
       <h1>Welcome to home page</h1>
+      <div className="list-key">
+        {(news.length > 0) && <ul className="list-new">
+          {
+            news.map(e => (
+              <li key={e.id}>
+                <img src={e.img} alt="thumb" />
+                <div className="news-content">
+                  <h3>{e.title}</h3>
+                  <p>{e.content}</p>
+                  <span className={`news-fav ${e.favs ? 'active' : ''}`} onClick={(event) => handleFav(event, e.id)}><FaHeart/></span>
+                </div>
+                <button type="button" className="icon-close" onClick={() => handleRemove(e.id)}>Delete</button>
+              </li>
+            ))
+          }
+        </ul>}
+        {(news.length === 0) && <ul className="list-new"><li>Bạn ko còn item nào.</li></ul>}
+      </div>
     </div>
-    // <div className="list-key">
-    //   {(data.length > 0) && <ul className="list-new">{newList}</ul>}
-    //   {(data.length === 0) && <ul className="list-new"><li>Bạn ko còn item nào.</li></ul>}
-    // </div>
+    
     
   )
 }
-
 
 export default Home;
--- react-app/src/pages/Features/index.js
+++ react-app/src/pages/Features/index.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import Home from './Home';
-import Category from './Category';
+import React, { Suspense, lazy } from 'react';
+// import Home from './Home';
+// import Category from './Category';
+// import Sale from './Sale';
+// import Contact from './Contact';
 import { Switch, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('./Home'));
+const Category = lazy(() => import('./Category'));
+const Sale = lazy(() => import('./Sale'));
+const Contact = lazy(() => import('./Contact'));
 
 const Features = () => {
   return (
     <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Switch>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+        <Route path="/sale">
+          <Sale/>
+        </Route>
         <Route path="/category">
           <Category/>
         </Route>
         <Route path="/">
           <Home/>
         </Route>
-        <Route path="/sale">
-          <Home/>
-        </Route>
-        <Route path="/contact">
-          <Home/>
-        </Route>
       </Switch>
+    </Suspense>
     </>
   );
 }
